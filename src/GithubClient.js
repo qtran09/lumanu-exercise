07edebd590560cc18a661466947b037a4992699a
Quyen Tran <qntran09@gmail.com>
2020-11-18 03:03:16 -0500

    first commit, debating on doing more css stuff

---
+++ src/GithubClient.js
@@ -0,0 +1,31 @@
+export class GithubClient
+{
+    static async AddRepo(owner, repo)
+    {
+        let repoList = JSON.parse(localStorage.getItem("repos"));
+        if(repoList === null) repoList = [];
+        const latestRelease = await this.GetLatestRelease(owner,repo);
+        if(latestRelease === null)
+        {
+            return false;
+        }
+        const toAdd = {repoOwner : owner, repoName : repo, lastReleaseSeen: latestRelease["tag_name"], lastReleaseDate :latestRelease["created_at"], hasRead: true};
+        repoList.push(toAdd);
+        localStorage.setItem("repos",JSON.stringify(repoList));
+        return true;
+    }
+    static async GetLatestRelease(owner, repo)
+    {
+            const response = await fetch('https://api.github.com/repos/' + owner + '/' + repo + '/releases/latest');
+            const json = await response.json();
+            if(json['message'] === 'Not Found') return null;
+            return json;
+
+    }
+
+    static async IsLatestVersion(owner, repo, version)
+    {
+        const latest = await this.GetLatestRelease(owner,repo);
+        return version === latest["tag_name"];
+    }
+}
---
+++ src/RepoListView.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import styles from './RepoListView.module.css';
+import { GithubClient } from './GithubClient.js';
+export class RepoListView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state =
+        {
+            addOwner: "",
+            addRepo: "",
+            repoList: []
+        };
+        this.refreshRepos = this.refreshRepos.bind(this);
+    }
+
+    async componentDidMount() {
+        let list = localStorage.getItem("repos");
+        if (list !== null) {
+            list = JSON.parse(list);
+            list.forEach(async element => {
+                let latestUpdate = await GithubClient.GetLatestRelease(element['repoOwner'], element['repoName']);
+                let hasSeen = await GithubClient.IsLatestVersion(element['repoOwner'], element['repoName'], element['lastReleaseSeen'])
+                if (!hasSeen) {
+                    element['lastReleaseSeen'] = latestUpdate['tag_name'];
+                    element['lastReleaseDate'] = latestUpdate['created_at'];
+                    element['hasRead'] = false;
+                    localStorage.setItem("repos", JSON.stringify(list));
+                }
+
+            });
+            this.setState({ repoList: JSON.parse(localStorage.getItem("repos")) });
+        }
+    }
+
+    onRepoAdd = async (e) => {
+        e.preventDefault();
+        let success = await GithubClient.AddRepo(this.state.addOwner, this.state.addRepo);
+        if (success) {
+            this.setState({ repoList: JSON.parse(localStorage.getItem("repos")) });
+        }
+        else {
+            alert("Could not add repo");
+        }
+
+    }
+
+
+    onRepoClick(repoObj) {
+        this.state.repoList.forEach(element => {
+            if (element === repoObj && !repoObj['hasRead']) {
+                element['hasRead'] = true;
+                localStorage.setItem("repos", JSON.stringify(this.state.repoList));
+            }
+        });
+        this.setState({ repoList: this.state.repoList });
+    }
+    refreshRepos() {
+        if (localStorage.getItem('repos') !== null) {
+            this.setState({ repoList: JSON.parse(localStorage.getItem("repos")) });
+        }
+    }
+
+    render() {
+        return (
+            <div className={styles.repo}>
+                <h3 className={styles.repoListTitle}>Repo List</h3>
+                <ul className={styles.repoList}>
+                    {this.state !== null && this.state.repoList !== null ? this.state.repoList.map((repoObj, index) => {
+                        let date = new Date(repoObj['lastReleaseDate']);
+                        return (
+                            <li key={index} className={styles.repoListItem} onClick={() => this.onRepoClick(repoObj)}>
+                                <div className={styles.repoBigText}>
+                                    {!repoObj['hasRead'] && "NEW"}
+                                </div>
+                                <div className={styles.repoBigText}>
+                                    {repoObj['lastReleaseSeen']}
+                                </div>
+                                <div className={styles.repoMediumText}>
+                                    {date.toDateString()}
+                                </div>
+                                <div className={styles.repoNameText}>
+                                    {repoObj["repoName"]}
+                                </div>
+                            </li>
+                        )
+                    }) : <></>
+                    }
+                </ul>
+                <div className={styles.refresh} onClick={this.refreshRepos}>Refresh Repo Data</div>
+                <h3 className={styles.addRepoTitle}>Add Repo</h3>
+                <form className={styles.addRepoForm} onSubmit={this.onRepoAdd}>
+                    <input type="text" placeholder="Owner" onChange={e => this.setState({ addOwner: e.target.value })} />
+                    <input type="text" placeholder="Repository" onChange={e => this.setState({ addRepo: e.target.value })} />
+                    <input type="submit" value="Add Repo" />
+                </form>
+            </div>
+        );
+    }
+}
